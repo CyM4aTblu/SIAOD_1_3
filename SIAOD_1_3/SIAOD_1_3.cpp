dbed56104fe50f145704a63ca8f9d50ced07541d
Дима <@LAPTOP-671DGP9F>
2022-09-08 10:54:50 +0300

    Добавьте файлы проекта.

---
+++ SIAOD_1_3/SIAOD_1_3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int bitMultiply(int i)
+{
+	i = i << 9;
+	return i;
+}
+int main()
+{
+	cout << "Pease, enter yout num: " << endl;
+	int n;
+	cin >> n;
+	cout << "After multiplying num by 512: " << bitMultiply(n);
+}
